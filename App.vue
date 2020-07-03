a6f84ff17e4d7c664a5c60e65584f2c3baa290be
yjn <13008062512@163.com>
2020-07-03 09:34:47 +0800

    style

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <date-picker />
+  </div>
+</template>
+
+<script>
+import DatePicker from "./DatePicker";
+export default {
+  components: {
+    DatePicker
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
